0c6d15a03697ca2b3b1d9cf8c4fb467b757e9e81
Manish <mp626329@gmail.com>
2021-12-27 19:54:05 +0530

    Fixed Nav Bar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Navbar = ()=> {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
               <Link className="navbar-brand" to="/">Navbar</Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
